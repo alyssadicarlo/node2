8b6f084ccea3516f8cdb6ab773ca32a12b626045
Alyssa DiCarlo <alyssa.dicarlo@gmail.com>
2021-06-15 12:45:27 -0700

    typo

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ const server = http.createServer((request, response) => {
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http:${hostname}:${port}`);
+    console.log(`Server running at http://${hostname}:${port}`);
 });
